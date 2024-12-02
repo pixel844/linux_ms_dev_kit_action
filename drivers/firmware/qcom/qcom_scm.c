d483ef321f4dbd9d8a0010cc9bffb932f43d38c4
Jens Glathe <jens.glathe@oldschoolsolutions.biz>
2024-12-02 20:41:30 +0100

    firmware: qcom: scm: Allow QSEECOM for HP Omnibook X14
    add "hp,omnibook-x14" as compatible device for QSEECOM
    
    This is required to get access to efivars and uefi boot loader support.
    
    Signed-off-by: Jens Glathe <jens.glathe@oldschoolsolutions.biz>

--- drivers/firmware/qcom/qcom_scm.c
+++ drivers/firmware/qcom/qcom_scm.c
@@ -1769,6 +1769,7 @@ EXPORT_SYMBOL_GPL(qcom_scm_qseecom_app_send);
  */
 static const struct of_device_id qcom_scm_qseecom_allowlist[] __maybe_unused = {
 	{ .compatible = "dell,xps13-9345" },
+	{ .compatible = "hp,omnibook-x14" },
 	{ .compatible = "lenovo,flex-5g" },
 	{ .compatible = "lenovo,thinkpad-t14s" },
 	{ .compatible = "lenovo,thinkpad-x13s", },
