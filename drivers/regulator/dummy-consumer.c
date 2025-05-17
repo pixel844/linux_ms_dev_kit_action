39c3f71b36dab5c758bc8ddd524ffc34ab91be69
Aleksandrs Vinarskis <alex.vinarskis@gmail.com>
2025-05-17 11:03:36 +0200

    fixup naming convention

--- drivers/regulator/dummy-consumer.c
+++ drivers/regulator/dummy-consumer.c
@@ -10,10 +10,10 @@
 #include <linux/pm.h>
 
 struct dummy_consumer_data {
-    struct regulator *dummy_vdd;
+    struct regulator *regulator;
 };
 
-static int dummy_probe(struct platform_device *pdev)
+static int dummy_consumer_probe(struct platform_device *pdev)
 {
     struct dummy_consumer_data *data;
     int ret;
@@ -22,13 +22,13 @@ static int dummy_probe(struct platform_device *pdev)
     if (!data)
         return -ENOMEM;
 
-    data->dummy_vdd = devm_regulator_get(&pdev->dev, "vdd");
-    if (IS_ERR(data->dummy_vdd)) {
+    data->regulator = devm_regulator_get(&pdev->dev, "vdd");
+    if (IS_ERR(data->regulator)) {
         dev_err(&pdev->dev, "Failed to get regulator\n");
-        return PTR_ERR(data->dummy_vdd);
+        return PTR_ERR(data->regulator);
     }
 
-    ret = regulator_enable(data->dummy_vdd);
+    ret = regulator_enable(data->regulator);
     if (ret) {
         dev_err(&pdev->dev, "Failed to enable regulator: %d\n", ret);
         return ret;
@@ -40,42 +40,42 @@ static int dummy_probe(struct platform_device *pdev)
     return 0;
 }
 
-static void dummy_remove(struct platform_device *pdev)
+static void dummy_consumer_remove(struct platform_device *pdev)
 {
     struct dummy_consumer_data *data = platform_get_drvdata(pdev);
-    regulator_disable(data->dummy_vdd);
+    regulator_disable(data->regulator);
 }
 
-static int dummy_suspend(struct device *dev)
+static int dummy_consumer_suspend(struct device *dev)
 {
     struct dummy_consumer_data *data = dev_get_drvdata(dev);
-    return regulator_disable(data->dummy_vdd);
+    return regulator_disable(data->regulator);
 }
 
-static int dummy_resume(struct device *dev)
+static int dummy_consumer_resume(struct device *dev)
 {
     struct dummy_consumer_data *data = dev_get_drvdata(dev);
-    return regulator_enable(data->dummy_vdd);
+    return regulator_enable(data->regulator);
 }
 
-static DEFINE_SIMPLE_DEV_PM_OPS(dummy_consumer_pm, dummy_suspend, dummy_resume);
+static DEFINE_SIMPLE_DEV_PM_OPS(dummy_consumer_pm, dummy_consumer_suspend, dummy_consumer_resume);
 
-static const struct of_device_id dummy_of_match[] = {
-    { .compatible = "dummy,regulator-consumer", },
+static const struct of_device_id dummy_consumer_of_match[] = {
+    { .compatible = "regulator-dummy-consumer", },
     { }
 };
-MODULE_DEVICE_TABLE(of, dummy_of_match);
+MODULE_DEVICE_TABLE(of, dummy_consumer_of_match);
 
-static struct platform_driver dummy_driver = {
+static struct platform_driver dummy_consumer_driver = {
     .driver = {
-        .name = "dummy-regulator-consumer",
-        .of_match_table = dummy_of_match,
+        .name = "regulator-dummy-consumer",
+        .of_match_table = dummy_consumer_of_match,
         .pm = pm_sleep_ptr(&dummy_consumer_pm),
     },
-    .probe = dummy_probe,
-    .remove = dummy_remove,
+    .probe = dummy_consumer_probe,
+    .remove = dummy_consumer_remove,
 };
-module_platform_driver(dummy_driver);
+module_platform_driver(dummy_consumer_driver);
 
 MODULE_AUTHOR("Aleksandrs Vinarskis");
 MODULE_LICENSE("GPL");
