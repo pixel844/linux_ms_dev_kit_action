a90c93525cfb66dfa4164f4a5a76e94163239c6b
Jens Glathe <jens.glathe@oldschoolsolutions.biz>
2025-11-14 21:46:56 +0100

    build: add rebuild_t14s.sh with tweaks for the thermal capacity of my T14s
    T14s is powerful, but the fan doesn't cool it enough for full load.
    Tone it down to the experimentally determined limits

---
+++ rebuild_t14s.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -ex
+
+export MAKEFLAGS="-j8 -l 6.5"
+export DEB_BUILD_OPTIONS="parallel=8"
+
+if ! diff -u debian/changelog debian.qcom-x1e/changelog
+then
+    fakeroot debian/rules clean
+fi
+
+dh_clean
+
+rm -rf debian/build/build-qcom-x1e/_____________________________________dkms/
+
+#export LLVM=1
+
+# Detect if we're on ARM64/AArch64
+if uname -m | grep -q 'aarch64\|arm64'; then
+  echo "Native ARM64 compilation detected"
+  time dpkg-buildpackage -d -b -k'jens.glathe@oldschoolsolutions.biz'
+else
+  echo "Cross-compiling to ARM64"
+  time dpkg-buildpackage -d -b -aarm64 -k'jens.glathe@oldschoolsolutions.biz'
+fi
+
+#time debuild --no-lintian -k'jens.glathe@oldschoolsolutions.biz'
